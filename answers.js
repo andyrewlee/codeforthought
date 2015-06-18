fe4bf186f371eaa89627c77f5c5a7a9b914bd90c
Andrew Lee <andyrewlee@gmail.com>
2015-06-18 16:06:12 -0700

    Remove answer to challenge 0, create function to add onto previous answers

--- answers.js
+++ answers.js
@@ -1,22 +1,33 @@
-var answer0 = ["<h1>Smiley Game</h1>", "<p id='score'>0</p>",
+var answer1 = ["<h1>Smiley Game</h1>", "<p id='score'>0</p>",
                "<img id='smiley' src='http://bit.ly/1erPIoe'>",
                "<img id='rabbit' src='http://bit.ly/1Ingp4K'>"];
 
-var answer1 = ["h1 {", "\tcolor: orange;", "}", "p {", "\tcolor: blue;", "}",
+var answer2 = ["h1 {", "\tcolor: orange;", "}", "p {", "\tcolor: blue;", "}",
                "img {", "\tposition: absolute", "}"];
 
-var answer2 = answer1.push("#smiley {", "\ttop: 200px;", "\tleft: 50px;", "}",
-                           "#rabbit {", "\ttop: 100px;", "\tleft: 300px;", "}");
-function showAnswer(num) {
-  if(num == 0) {
-    html_editor.setValue(answer0.join("\n"));
-  }
+var answer3 = addToAnswer(answer2, ["#smiley {", "\ttop: 200px;", "\tleft: 50px;",
+              "}", "#rabbit {", "\ttop: 101px;", "\tleft: 300px;", "}"]);
 
+function addToAnswer(oldAnswer, arr) {
+  var newAnswer = [];
+  newAnswer = newAnswer.concat.apply(newAnswer, oldAnswer);
+  for(var i = 0; i < arr.length; i++) {
+    newAnswer.push(arr[i]);
+  }
+  return newAnswer;
+}
+
+function showAnswer(num) {
   if(num == 1) {
-    css_editor.setValue(answer1.join("\n"));
+    html_editor.setValue(answer1.join("\n"));
   }
 
   if(num == 2) {
     css_editor.setValue(answer2.join("\n"));
+  }
+
+  if(num == 3) {
+    console.dir(answer3);
+    css_editor.setValue(answer3.join("\n"));
   }
 }
